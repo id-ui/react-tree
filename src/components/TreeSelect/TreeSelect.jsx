9443d0ac782cb40e344c894a6d7f6445eb7e86d8
kseniya <kaprisa57@gmail.com>
2020-12-08 17:35:17 +0300

    remove extra files

--- src/components/TreeSelect/TreeSelect.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function TreeSelect() {
-  return <div></div>;
-}
-
-export default TreeSelect;
--- src/index.js
+++ src/index.js
@@ -4,4 +4,3 @@ export {
   CheckboxTreeLeaf,
 } from './components/CheckboxTree';
 export { default as TreeNode } from './components/TreeNode';
-export { default as TreeSelect } from './components/TreeSelect';
